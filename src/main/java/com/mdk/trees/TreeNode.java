8827e1ab43c1c1d6a6a9a509f086c22be1a78e81
Mahmoud Dkhissi <mahmouddkhissi68@gmail.com>
2026-01-13 23:56:58 +0100

    isSubtree (Recursive + Iterative) / TreeNode

--- src/main/java/com/mdk/trees/TreeNode.java
+++ src/main/java/com/mdk/trees/TreeNode.java
@@ -65,5 +65,52 @@ public class TreeNode {
         }
     }
 
+    public boolean isSubtreeRecursive(TreeNode root, TreeNode subRoot) {
+        if (root!=null && subRoot!=null) {
+            if(root.val == subRoot.val) {
+                if(isSameTreeRecursive(root,subRoot)) {
+                    return true;
+                }
+                return isSubtreeRecursive(root.right,subRoot) || isSubtreeRecursive(root.left,subRoot);
+            }
+            else {
+                return isSubtreeRecursive(root.right,subRoot) || isSubtreeRecursive(root.left,subRoot);
+            }
+        }
+        else if (root== null && subRoot==null) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean isSubtreeIterative(TreeNode root, TreeNode subRoot) {
+          Stack<TreeNode> stack = new Stack<>();
+          if (root==null){
+              return subRoot==null;
+          }
+          if (subRoot==null) {
+              return false;
+          }
+          stack.push(root);
+          while (!stack.isEmpty()) {
+              TreeNode node = stack.pop();
+              if (node.val== subRoot.val && isSameTreeRecursive(node,subRoot)) {
+                  return true;
+              }
+              else {
+                  if (node.right!=null) {
+                      stack.push(node.right);
+                  }
+                  if (node.left!=null) {
+                      stack.push(node.left);
+                  }
+              }
+
+          }
+          return false;
+    }
+
   }
 
